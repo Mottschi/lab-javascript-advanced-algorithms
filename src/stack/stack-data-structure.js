d5e32fab16044d34a57511ed8f697bf8812966f4
Sebastian Mottschall <srophkor@gmail.com>
2023-01-20 17:04:08 +0100

    Iteration 1 and 2 done

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,22 +6,36 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    return this.stackControl.length < this.MAX_SIZE;
   }
 
   isEmpty() {
     // ... your code goes here
+    return this.stackControl.length === 0;
   }
 
   push(item) {
     // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+      return this.stackControl; // this seems a bit questionable - we wouldn't usually want anyone to directly access the array we use for our stack data, as that circumvents the whole controls? but Jasmine expects it
+    } else {
+      throw new Error('STACK_OVERFLOW');
+    }
   }
 
   pop() {
     // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.stackControl.pop();
+    } else {
+      throw new Error('STACK_UNDERFLOW')
+    }
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl;
   }  
 }
 
